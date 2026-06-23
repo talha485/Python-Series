717f7056c1f036ecdc28c922c268c0a3de244746
talha485 <talhachaudhary120@gmail.com>
2026-06-23 17:32:45 +0500

    done problem with string cases

---
+++ stringcases.py
@@ -0,0 +1,9 @@
+name = str(input("Enter your name "))
+upper = name.upper()
+lower = name.lower()
+length = len(name)
+reversed_name = name[::-1]
+print(upper)
+print(lower)
+print(length)
+print(reversed_name)
