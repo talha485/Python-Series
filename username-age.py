a2358bbbea50ecac9794390950a9a8cc9f5e58ae
talha485 <talhachaudhary120@gmail.com>
2026-06-23 16:20:21 +0500

    done with day 1 of python

---
+++ username-age.py
@@ -0,0 +1,3 @@
+name = str(input("Enter your name :"))
+age = int(input("Enter your age :"))
+print(name , age)
---
+++ variables.py
@@ -0,0 +1,11 @@
+age = 22
+price = 99.99
+name = "Talha"
+is_student = True
+data = None
+
+print(age, type(age))
+print(price, type(price))
+print(name, type(name))
+print(is_student, type(is_student))
+print(data, type(data))
